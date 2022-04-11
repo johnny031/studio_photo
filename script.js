637cc7e54e364895f365a0d2f6ad63cb79b8a39c
Johnny <johnny055238@gmail.com>
2022-04-11 14:02:16 +0800

    use six img tag

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ $(".img").css("width", vmin)
 $(".right-btn").click(function (e) {
     if (current_img >= 5) return false
     current_img++
-    $(".img").attr("src", `https://drive.google.com/uc?export=view&id=${img_id_list[current_id][current_img]}`)
+    $(".current_img").removeClass("current_img")
+    $(`#img${current_img}`).addClass("current_img")
     $(".annotation").html(annotation_list[current_id][current_img])
 })
 
 $(".left-btn").click(function (e) {
     if (current_img <= 0) return false
     current_img--
-    $(".img").attr("src", `https://drive.google.com/uc?export=view&id=${img_id_list[current_id][current_img]}`)
+    $(".current_img").removeClass("current_img")
+    $(`#img${current_img}`).addClass("current_img")
     $(".annotation").html(annotation_list[current_id][current_img])
 })
 
@@ -24,7 +26,10 @@ $(".btn").click(function () {
     $(".overlay").show()
     $("body").css("background-color", "rgba(0, 0, 0, 0.8)")
     $(".img-wrapper").css("display", "flex")
-    $(".img").attr("src", `https://drive.google.com/uc?export=view&id=${img_id_list[current_id][0]}`)
+    for (let i = 0; i < 6; i++) {
+        $(`#img${i}`).attr("src", `https://drive.google.com/uc?export=view&id=${img_id_list[current_id][i]}`)
+    }
+    $("#img0").addClass("current_img")
     $(".annotation").html(annotation_list[current_id][0])
 })
 
